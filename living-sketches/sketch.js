3eabeff184472ba614878b6f0f3ff4f74493254f
kashif-08 <kashif.designspace@gmail.com>
2026-03-20 16:47:41 +0800

    changes to living sketch

--- living-sketches/sketch.js
+++ living-sketches/sketch.js
@@ -1,7 +1,5 @@
 let landscape = [];
 let clouds = [];
-let movingBus;
-let movingClouds;
 
 let busAnim = 0;
 let cloudAnim = 0;
@@ -18,8 +16,6 @@ function preload() {
 
 function setup() {
   createCanvas(1440, 1024);
-  movingBus = landscape;
-  movingClouds = clouds;
 }
 
 function draw() {
@@ -27,27 +23,27 @@ function draw() {
 
 
   image(
-    movingBus[busAnim],
+    landscape[busAnim],
     0,
     0,
-    movingBus[0].width * .6,
-    movingBus[0].height * .6
+    landscape[0].width * .6,
+    landscape[0].height * .6
   );
 
-  busAnim = floor((frameCount / 20) % movingBus.length);
+  busAnim = floor((frameCount / 20) % landscape.length);
 
   image(
-    movingClouds[cloudAnim],
+    clouds[cloudAnim],
     0,
     0,
-    movingClouds[0].width * .6,
-    movingClouds[0].height * .6
+    clouds[0].width * .6,
+    clouds[0].height * .6
   );
   moveClouds ()
 }
 
   function moveClouds () {
     if (mouseIsPressed)
-    cloudAnim = floor((frameCount / 20) % movingClouds.length);
+    cloudAnim = floor((frameCount / 20) % clouds.length);
 
   }
