c137230433a540470fc42b42c6bb8b5627e10a67
kashif-08 <kashif.designspace@gmail.com>
2026-04-27 23:52:50 +0800

    updates in project b

--- project-b/sketch.js
+++ project-b/sketch.js
@@ -1,40 +1,222 @@
-//image related variables
+// image related variables
 let settingImage;
+let settingImageSnow;
 let boat;
 let birds;
 let birdX;
+let snowflakes = [];
+let marketImg;
+
+//overlay/opening img
+let openingImg;
+let isOpening = true;
+let enterBtn;
 
 // sound related variables
 let marketSound;
+let shikaraSound;
+let hazratbal;
+let dalgateSound;
+let song1;
+let song2;
+let song3;
+let song4;
+
+// season toggle
+let isSnow = false;
+let toggleBtn;
+
+// music player
+let currentSong = 0;
+let isPlaying = false;
+let playBtn;
+let nextBtn;
+let prevBtn;
+let songNames = ['Hoshdar', 'Hoshmatiye', 'Golabaw', 'Jhelum'];
 
 function preload() {
+  openingImg = loadImage('images/opening.png');
   settingImage = loadImage('images/main-setting.png');
+  settingImageSnow = loadImage('images/main-setting-snow.png');
+  marketImg = loadImage('images/goni-khan-market.jpg');
   birds = loadImage('images/birds.gif');
   marketSound = loadSound('audios/market.mp3');
+  shikaraSound = loadSound('audios/Shikara.mp3');
+  hazratbal = loadSound('audios/Hazratbal.mp3');
+  dalgateSound = loadSound('audios/Dal-gate.wav');
+  song1 = loadSound('audios/Hoshdar.mp3');
+  song2 = loadSound('audios/Hoshmatiye.mp3');
+  song3 = loadSound('audios/Golabaw.mp3');
+  song4 = loadSound('audios/Jhelum.mp3');
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  boat = new Boat(0, 200, 2);
+  boat = new Boat(130, 280, 2);
   birdX = 1000;
   birds.resize(300, 0);
+
+  for (let i = 0; i < 100; i++) {
+    snowflakes[i] = new Snowflake();
+  }
+
+  //entry button
+
+  enterBtn = createButton('Enter');
+  enterBtn.position(windowWidth / 2 - 50, windowHeight / 2 + 100);
+  enterBtn.style('padding', '12px 30px');
+  enterBtn.style('font-size', '18px');
+  enterBtn.style('cursor', 'pointer');
+  enterBtn.style('border', 'none');
+  enterBtn.style('border-radius', '8px');
+  enterBtn.style('background-color', 'rgba(0,0,0,0.5)');
+  enterBtn.style('color', 'white');
+  enterBtn.mousePressed(function () {
+    isOpening = false;
+    enterBtn.remove();
+    dalgateSound.loop();
+  });
+
+  // toggle button
+  toggleBtn = createButton('❄️');
+  toggleBtn.position(20, 20);
+  toggleBtn.style('padding', '10px 16px');
+  toggleBtn.style('font-size', '16px');
+  toggleBtn.style('cursor', 'pointer');
+  toggleBtn.style('border', 'none');
+  toggleBtn.style('border-radius', '8px');
+  toggleBtn.style('background-color', '#4a90d9');
+  toggleBtn.style('color', 'white');
+  toggleBtn.mousePressed(toggleSeason);
+
+  // prev button
+  prevBtn = createButton('‹');
+  prevBtn.position(windowWidth - 120, windowHeight - 60);
+  prevBtn.style('font-size', '24px');
+  prevBtn.style('cursor', 'pointer');
+  prevBtn.style('border', 'none');
+  prevBtn.style('border-radius', '8px');
+  prevBtn.style('background-color', 'rgba(0,0,0,0.5)');
+  prevBtn.style('color', 'white');
+  prevBtn.style('width', '32px');
+  prevBtn.style('height', '32px');
+  prevBtn.mousePressed(function () {
+    currentSong = (currentSong - 1 + 4) % 4;
+    if (isPlaying) {
+      playSong(currentSong);
+    }
+    playBtn.html('⏸');
+  });
+
+  // play button
+  playBtn = createButton('▶');
+  playBtn.position(windowWidth - 170, windowHeight - 60);
+  playBtn.style('font-size', '14px');
+  playBtn.style('cursor', 'pointer');
+  playBtn.style('border', 'none');
+  playBtn.style('border-radius', '8px');
+  playBtn.style('background-color', 'rgba(0,0,0,0.5)');
+  playBtn.style('color', 'white');
+  playBtn.style('width', '32px');
+  playBtn.style('height', '32px');
+  playBtn.mousePressed(function () {
+    if (isPlaying) {
+      song1.stop(); song2.stop(); song3.stop(); song4.stop();
+      isPlaying = false;
+      playBtn.html('▶');
+    } else {
+      playSong(currentSong);
+      isPlaying = true;
+      playBtn.html('⏸');
+    }
+  });
+
+  // next button
+  nextBtn = createButton('›');
+  nextBtn.position(windowWidth - 85, windowHeight - 60);
+  nextBtn.style('font-size', '24px');
+  nextBtn.style('cursor', 'pointer');
+  nextBtn.style('border', 'none');
+  nextBtn.style('border-radius', '8px');
+  nextBtn.style('background-color', 'rgba(0,0,0,0.5)');
+  nextBtn.style('color', 'white');
+  nextBtn.style('width', '32px');
+  nextBtn.style('height', '32px');
+  nextBtn.mousePressed(function () {
+    currentSong = (currentSong + 1) % 4;
+    if (isPlaying) {
+      playSong(currentSong);
+    }
+    playBtn.html('⏸');
+  });
+}
+
+function playSong(index) {
+  let all = [song1, song2, song3, song4];
+  for (let i = 0; i < all.length; i++) {
+    all[i].stop();
+  }
+  all[index].play();
+}
+
+function toggleSeason() {
+  isSnow = !isSnow;
+  if (isSnow) {
+    toggleBtn.html('☀️');
+    toggleBtn.style('background-color', '#6ab0e8');
+  } else {
+    toggleBtn.html('❄️');
+    toggleBtn.style('background-color', '#4a90d9');
+  }
 }
 
 function draw() {
   background(220);
-  image(settingImage, 0, 0, width, height);
-  image(birds, birdX, 0);
-  birdX--;
-
-  boat.move();
-  boat.display();
-
-  if (mouseX > 855 && mouseX < 1500 && mouseY > 340 && mouseY < 520) {
-    if (!marketSound.isPlaying()) {
-      marketSound.play();
-    }
+  if (isOpening) {
+    image(openingImg, 0, 0);
   } else {
-    marketSound.stop();
+
+    // background based on toggle
+    if (isSnow) {
+      image(settingImageSnow, 0, 0, width, height);
+      for (let i = 0; i < snowflakes.length; i++) {
+        snowflakes[i].move();
+        snowflakes[i].show();
+      }
+    } else {
+      image(settingImage, 0, 0, width, height);
+    }
+
+    image(birds, birdX, 0);
+    birdX--;
+
+    boat.move();
+    boat.display();
+    boat.hover();
+
+    if (mouseX > 855 && mouseX < 1500 && mouseY > 340 && mouseY < 450) {
+      if (!marketSound.isPlaying()) {
+        marketSound.play();
+      }
+    } else {
+      marketSound.stop();
+    }
+
+    if (mouseX > 313 && mouseX < 425 && mouseY > 191 && mouseY < 315) {
+      if (!hazratbal.isPlaying()) {
+        hazratbal.play();
+      }
+    } else {
+      hazratbal.stop();
+    }
+
+    // song name above player
+    if (isPlaying) {
+      fill(255);
+      noStroke();
+      textSize(14);
+      text(songNames[currentSong], windowWidth - 120, windowHeight - 70);
+    }
   }
 }
 
@@ -44,7 +226,7 @@ class Boat {
     this.x = x;
     this.y = y;
     this.speed = speed;
-    this.img = loadImage('images/BOATS.png');
+    this.img = loadImage('images/moving-shikara.png');
   }
 
   move() {
@@ -54,7 +236,45 @@ class Boat {
     }
   }
 
+  hover() {
+    if (mouseX > this.x && mouseX < this.x + 230 && mouseY > this.y + 300 && mouseY < this.y + 370) {
+      if (!shikaraSound.isPlaying()) {
+        shikaraSound.play();
+      }
+    } else {
+      shikaraSound.stop();
+    }
+  }
+
   display() {
-    image(this.img, this.x, this.y + random(300, 300.8), this.img.width, this.img.height);
+    noFill();
+    stroke(255, 0, 0);
+    strokeWeight(2);
+    rect(this.x, this.y + 300, this.img.width, this.img.height);
+    image(this.img, this.x, this.y + random(300, 300.8));
+  }
+}
+
+
+class Snowflake {
+  constructor() {
+    this.x = random(width);
+    this.y = random(-height, height);
+    this.size = random(4, 10);
+    this.speed = random(3, 7);
+  }
+
+  move() {
+    this.y += this.speed;
+    if (this.y > height) {
+      this.y = 0;
+      this.x = random(width);
+    }
+  }
+
+  show() {
+    noStroke();
+    fill(255, 255, 255, 200);
+    circle(this.x, this.y, this.size);
   }
 }
